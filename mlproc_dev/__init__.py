884574d2ba4ef062362374ed473811ba6be1cf40
Jeremy Magland <jeremy.magland@gmail.com>
2018-07-27 17:05:15 -0400

    spikeforest_dev

---
+++ mlproc_dev/__init__.py
@@ -0,0 +1,5 @@
+from .mlproc_impl import runProcess
+from .mlproc_impl import setCurrentClient,addProcess
+from .mlproc_impl import spec
+from .mlproc_impl import locateFile,realizeFile
+from .mlclient import MLClient
