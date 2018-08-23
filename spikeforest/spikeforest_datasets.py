6bc51ca03ddb572b10f4161aa605dfda62bdc6c5
Jeremy Magland <jeremy.magland@gmail.com>
2018-08-23 12:00:06 -0400

    update examples

--- spikeforest/spikeforest_datasets.py
+++ spikeforest/spikeforest_datasets.py
@@ -2,57 +2,63 @@ from mountainlab_pytools import mlproc as mlp
 from jp_ephys_viz import ephys_viz_v1
 import ipywidgets as widgets
 
-def all_datasets():
+def all_datasets(verbose=False):
     datasets=[]
-    datasets.append({
-        "id":"synth_tetrode_30min",
-        "raw_path":"kbucket://b5ecdf1474c5/datasets/synth_datasets/datasets/synth_tetrode_30min"
-    })
-    datasets.append({
-        "id":"synth_tetrode_120min",
-        "raw_path":"kbucket://b5ecdf1474c5/datasets/synth_datasets/datasets/synth_tetrode_120min"
-    })
-    datasets.append({
-        "id":"synth_16ch_30min",
-        "raw_path":"kbucket://b5ecdf1474c5/datasets/synth_datasets/datasets/synth_16ch_30min"
-    })
-
-    # bionet_8x
-    datasets.append({
-        "id":"bionet_8x/static_8x_A_2A",
-        "raw_path":"kbucket://4c21262c8704/static_8x_A_2A"
-    })
-
-    #datasets.append({
-    #    "id":"synth_tetrode_30min",
-    #    "raw_path":"kbucket://b5ecdf1474c5/datasets/synth_datasets/datasets/synth_tetrode_30min"
-    #});
-    #datasets.append({
-    #    "id":"synth_tetrode_120min",
-    #    "raw_path":"kbucket://b5ecdf1474c5/datasets/synth_datasets/datasets/synth_tetrode_120min"
-    #});
-    #datasets.append({
-    #    "id":"synth_16ch_30min",
-    #    "raw_path":"kbucket://b5ecdf1474c5/datasets/synth_datasets/datasets/synth_16ch_30min"
-    #});
+    
+    groups=[
+        dict(
+            group_dir='kbucket://b5ecdf1474c5/datasets/synth_datasets/datasets',
+            group_name='synth'
+        ),
+        dict(
+            group_dir='kbucket://697623ec3681/kampff',
+            group_name='kampff'
+        ),
+        dict(
+            group_dir='kbucket://697623ec3681/boyden',
+            group_name='boyden'
+        ),
+        dict(
+            group_dir='kbucket://697623ec3681/bionet_8x',
+            group_name='bionet_8x'
+        ),
+        dict(
+            group_dir='kbucket://697623ec3681/mea256yger',
+            group_name='mea256yger'
+        )
+    ]
+    #groups=[groups[0]]
+    
+    for group in groups:
+        group_dir=group['group_dir']
+        group_name=group['group_name']
+        if verbose:
+            print('Reading '+group_dir);
+        D=mlp.readDir(group_dir)
+        for name in D['dirs']:
+            datasets.append({
+                "id":group_name+"--"+name,
+                "raw_path":group_dir+'/'+name
+            })
     
     return datasets
 
-def find_dataset(id):
-    datasets=all_datasets()
-    for ds in datasets:
-        if ds['id']==id:
-            return ds
-    return None
-
-def view_dataset(dsdir,external_link=False,height=450,dataset_id='',firings=''):
+def view_dataset(dsdir,external_link=False,height=450,dataset_id='',firings='',visible_channels=None):
     params={'view':'dataset','dataset':dsdir}
     if firings:
         params['firings']=mlp.kbucketPath(firings)
+    if visible_channels:
+        if type(visible_channels)==list:
+            visible_channels=','.join(str(x) for x in visible_channels)
+        params['visible_channels']=visible_channels
     ephys_viz_v1(params=params,title='Dataset: {}'.format(dataset_id),external_link=external_link,height=height)
     
-def dataset_selection_box():
-    datasets=all_datasets()
+def dataset_selection_widget():
+    out=widgets.Output()
+    display(out)
+    with out:
+        datasets=all_datasets(verbose=True)
+    out.close()
     dataset_ids=[]
     for ds in datasets:
         dataset_ids.append(ds['id'])
@@ -64,4 +70,11 @@ def dataset_selection_box():
         #description='Select a dataset:',
         disabled=False
     )
+    def get_selected_dataset():
+        id=SS.value
+        for ds in datasets:
+            if ds['id']==id:
+                return ds
+        return None
+    SS.selectedDataset=get_selected_dataset
     return SS
